308f40f1dd03521462f386d697a080ed2aecc09d
Nasrin <saefnasrin@gmail.com>
2024-05-29 10:45:00 +0200

    chore: add docstrings

--- leichte_sprache/dataset/transform_singular_dataset.py
+++ leichte_sprache/dataset/transform_singular_dataset.py
@@ -1,14 +1,22 @@
 import datasets
-from transformers import (
-    PreTrainedTokenizer,
-)
 
 from leichte_sprache.utils.model_utils import load_pipeline, generate
 
 
-def create_prompt(row, tokenizer: PreTrainedTokenizer):
-    # todo docstring
-    text = row["text"]
+def create_prompt(row, text_col_name: str = "text", return_col_name: str = "prompts"):
+    """Create a prompt in the chat message format from the column containing the text.
+    Adds a system prompt explaining the linguistic properties of "Standard-Deutsch" and
+    a user prompt asking to translate the Leichte Sprache into Standard-Deutsch.
+    Returns a dictionary with the return column name as its key and the chat messages list
+    as its value.
+
+    :param row: row of a HF dataset
+    :param text_col_name: name of the column containing the text to be translated. Default: "text"
+    :param return_col_name: name of the new column with the prompt. Default: "prompts"
+    :return: dict {return_col_name: chat_messages}
+    """
+
+    text = row[text_col_name]
     messages = [
         {
             "role": "system",
@@ -19,27 +27,32 @@ def create_prompt(row, tokenizer: PreTrainedTokenizer):
             "content": f"Schreibe den folgenden Text für ein gebildetes Publikum um. Halte dich an die im Text genannten Infomationen; Erläuterungen gängiger Begriffe können wegfallen. Text:\n{text}\nText im standardsprachlichen Stil:",
         },
     ]
-    # todo constants for column names
-    return {"prompts": messages}
+    return {return_col_name: messages}
 
 
-def load_dataset(dataset_path: str, tokenizer: PreTrainedTokenizer) -> datasets.Dataset:
-    # todo: docstring
+def load_and_prepare_dataset(dataset_path: str) -> datasets.Dataset:
+    """Load dataset from a CSV file and create prompts for each row from its "text" column.
+
+    :param dataset_path: path to the CSV file
+    :return: HF Dataset based on the CSV file, with added "prompts" column
+    """
     dataset = datasets.load_dataset("csv", data_files=dataset_path, split="train")
-    dataset = dataset.map(create_prompt, fn_kwargs={"tokenizer": tokenizer})
+    dataset = dataset.map(create_prompt)
     return dataset
 
 
 def transform_singular_dataset():
     # todo: CLI
+    # todo: load from database
     dataset_path = "data/datasets/dataset_singular.csv"
     model_id = "DiscoResearch/Llama3_DiscoLM_German_8b_v0.1_experimental"
 
     pipe = load_pipeline(model_id=model_id)
-    dataset = load_dataset(dataset_path=dataset_path, tokenizer=pipe.tokenizer)
+    dataset = load_and_prepare_dataset(dataset_path=dataset_path)
     results = generate(dataset=dataset, pipe=pipe)
 
     print(results)  # todo: remove print
+    # todo: save to database
     results.to_csv("data/datasets/dataset_singular_translated.csv")
 
 
