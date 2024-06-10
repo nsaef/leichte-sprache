fbd2fe6b976c7d191da6a9ca4ee42e32fd549a5f
Nasrin <saefnasrin@gmail.com>
2024-06-10 15:12:31 +0200

    chore: fix versions

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 # set up environment
-conda create -n leichte_sprache python=3.11
+conda create -y -n leichte_sprache python=3.9
 conda activate leichte_sprache
-pip install -r requirements.txt
+pip install -e .
 
 # install pre-commit hook
 pre-commit install
+
+# install German lcoale (needed for crawling)
+sudo locale-gen de_DE.utf8
