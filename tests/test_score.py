09270acd31b3cedd354952d2f6d1cbac47ccede6
Nasrin <saefnasrin@gmail.com>
2024-06-13 21:58:50 +0200

    feat: calculate avg word length in text

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,6 +1,6 @@
 import pytest
 
-from leichte_sprache.evaluation.score import count_words_naive
+from leichte_sprache.evaluation.score import split_text_naive, calc_mean_word_length
 
 
 @pytest.mark.parametrize(
@@ -14,7 +14,27 @@ from leichte_sprache.evaluation.score import count_words_naive
         ["Combine: diff-erent parameters", ":", True, 3],
     ),
 )
-def test_count_words_naive(text, sep, separate_hyphens, expected):
-    words = count_words_naive(text=text, sep=sep, separate_hyphens=separate_hyphens)
-    assert words == expected
+def test_count_words_naive(text: str, sep: str, separate_hyphens: bool, expected: int):
+    words = split_text_naive(text=text, sep=sep, separate_hyphens=separate_hyphens)
+    assert len(words) == expected
+    return
+
+
+@pytest.mark.parametrize(
+    "text, sep, separate_hyphens, expected",
+    (
+        ["foo bar", None, True, 3.0],  # default case
+        ["foo - bar", None, True, 3.0],  # hyphen with whitespace
+        ["This sent is hyphen-ated", None, True, 4.0],  # test hyphen separation
+        ["This sent is hyphen-ated", None, False, 5.25],  # test hyphen separation
+        ["Split at: a different char", ":", True, 12.5],
+        ["Combine: diff-erent parameters", ":", True, 9.33],
+    ),
+)
+def test_calc_mean_word_length(
+    text: str, sep: str, separate_hyphens: bool, expected: float
+):
+    words = split_text_naive(text=text, sep=sep, separate_hyphens=separate_hyphens)
+    avg_len = calc_mean_word_length(words)
+    assert round(avg_len, 2) == expected
     return
